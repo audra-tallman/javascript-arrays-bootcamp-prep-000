41149da9555550dc85dc937c659ca164d135886c
Audra Tallman <audra.tallman@hotmail.com>
2019-02-11 00:48:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,14 +24,16 @@ function accessElementInArray (array, index){
     return array[2];
 }
 function destructivelyRemoveElementFromBeginningOfArray (array){ 
-  return array.shift();
+  array.shift();
+  return array
 }
 function removeElementFromBeginningOfArray (array){
   array = [1, 2, 3];
     return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray (array){
-    return array.pop();  
+  array.pop();
+  return array;
 }
 function removeElementFromEndOfArray(array) {
 return array.slice(0, array.length - 1);
