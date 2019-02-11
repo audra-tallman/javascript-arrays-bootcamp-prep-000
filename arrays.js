8cf5b8351828ab219e8e4c449058a6837e2902c4
Audra Tallman <audra.tallman@hotmail.com>
2019-02-11 00:40:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,9 +31,8 @@ function removeElementFromBeginningOfArray (array){
     return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray (array){
-    return array.pop(3);  
+    return array.pop();  
 }
-
 function removeElementFromEndOfArray(array) {
 return array.slice(0, array.length - 1);
 }
